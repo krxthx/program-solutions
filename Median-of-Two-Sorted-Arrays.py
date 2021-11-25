13365590b92800c8c6842985aece94ac504bc5e3
Krithika N <76687631+krithika117@users.noreply.github.com>
2021-11-25 21:35:24 +0530

    Create Median-of-Two-Sorted-Arrays.py

---
+++ Median-of-Two-Sorted-Arrays.py
@@ -0,0 +1,16 @@
+4. Median of Two Sorted Arrays
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        res = 0
+        nums = nums1+nums2
+        nums.sort()
+        
+        if len(nums)%2!=0:
+            res = nums[(len(nums))//2]
+            return res
+        elif len(nums)%2==0:
+            res = nums[len(nums)//2]+nums[(len(nums)//2)-1]
+            return res/2
+            
