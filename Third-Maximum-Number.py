f0e7511fe22a7b547b71a0bc9ca138d9b9b7dc2e
Krithika N <76687631+krithika117@users.noreply.github.com>
2021-11-25 22:46:49 +0530

    Create Third-Maximum-Number.py

---
+++ Third-Maximum-Number.py
@@ -0,0 +1,14 @@
+
+# 414. Third Maximum Number
+# LeetCode
+# Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        nums.sort(reverse=True)
+        if len(nums)>2:
+            return nums[2]
+        else:
+            return max(nums)
+        
