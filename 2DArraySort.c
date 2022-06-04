a6f7055e8e5c7e975dad7e6e1b9bc4eeebcb047b
Krithika N <76687631+krithika117@users.noreply.github.com>
2022-06-04 09:59:01 +0530

    Update and rename 2DArray Sort.c to 2DArraySort.c

--- 2DArraySort.c
+++ 2DArraySort.c
@@ -21,10 +21,9 @@ int main()
             }
         }
     }
-    for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                printf("%d ",a[i][j]);
-            }
+    for(int i=0;i<9;i++){
+        printf("%d ",a[0][i]);
+        if((i+1)%3==0)
             printf("\n");
     }
     
