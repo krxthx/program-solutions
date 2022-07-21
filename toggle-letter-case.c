069534797e27b67e2f7753708c93d6ff03543ce3
Krithika N <76687631+krithika117@users.noreply.github.com>
2022-07-21 08:46:40 +0530

    Update toggle-letter-case.c

--- toggle-letter-case.c
+++ toggle-letter-case.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#include <string.h>
-
 int main()
 {
     char var[50];
     scanf("%[^\n]s",var);
-    for(int i=0;i<strlen(var);i++){
+    for(int i=0;var[i]!=0;i++){
       if((var[i]>=97) && (var[i]<=122))  
         var[i]-=32;
       else if((var[i]>=65) && (var[i]<=90))  
@@ -14,3 +12,7 @@ int main()
     printf("%s",var);
     return 0;
 }
+
+
+// ip: hElLo WoRLD
+// op: HeLlO wOrld
